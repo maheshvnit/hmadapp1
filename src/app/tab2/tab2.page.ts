e2a8be81c63917632065cb63b3155570d4bf923a
Mahesh Prasad <mahesh.vrce@gmail.com>
2024-03-05 12:03:57 +0530

    delete photo and live reload

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -8,7 +8,9 @@ import { CommonModule } from '@angular/common';
 // import { FormComponentComponent } from './form-component/form-component.component';
 //import { IonicModule } from '@ionic/angular';
 
-import { PhotoService } from '../services/photo.service';
+import { ActionSheetController } from '@ionic/angular';
+
+import { PhotoService, UserPhoto } from '../services/photo.service';
 
 @Component({
   selector: 'app-tab2',
@@ -20,7 +22,8 @@ import { PhotoService } from '../services/photo.service';
 })
 export class Tab2Page {
 
-  constructor(public photoService: PhotoService) { }
+  constructor(public photoService: PhotoService,
+    public actionSheetController: ActionSheetController) {}
 
   async ngOnInit() {
     await this.photoService.loadSaved();
@@ -29,5 +32,28 @@ export class Tab2Page {
   addPhotoToGallery() {
     this.photoService.addNewToGallery();
   }
+
+  public async showActionSheet(photo: UserPhoto, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+          }
+      }]
+    });
+    await actionSheet.present();
+  }
+
 
 }
